3dcd7e30092b49855cec62443878b4bbaf1c9328
Maharaj Syed <maharajsyed@yahoo.com>
2020-01-29 01:05:05 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,8 @@
 def turn_count(board)
   counter = 0 
-turn_count each.do |user_input|
-  if user_input == "X" || user_input == "O"
-  counter += 1 
-  end
+  turn_count each.do |user_input|
+    if user_input == "X" || user_input == "O"
+    counter += 1 
+  end 
+    end
 end 
